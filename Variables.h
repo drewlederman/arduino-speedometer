21bdb7ac0cb13510c1f8fac7f8a9e599576570f4
Drew Lederman <drew@directlocalfood.com>
2015-03-08 15:57:19 -0600

    Initial commit

---
+++ Variables.h
@@ -0,0 +1,23 @@
+// The last time the hall effect sensor was triggered
+volatile unsigned long lastTrigger = 0;
+
+// Rotations per second, set by the ISR
+volatile float rps = 0.0;
+
+// Odometer value
+volatile float totalMiles = 0.0;
+
+// Current mode
+int mode = MODE_SPEEDOMETER;
+
+// The last time the odometer value was written to memory
+unsigned long lastOdometerWrite = 0;
+
+// Used to control the LED display
+Adafruit_AlphaNum4 alpha = Adafruit_AlphaNum4();
+
+// Helper class for handling button presses
+Button button = Button(pinButton, LOW, 3000);
+
+// Helper class for running code at intervals
+Timer timer = Timer();
